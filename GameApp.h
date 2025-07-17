c451bece99649a52e2d1b6e3e556635f07379fa9
therzen <3467896930@qq.com>
2025-07-17 10:58:36 +0800

    Update GameApp.h

--- GameApp.h
+++ GameApp.h
@@ -2,10 +2,10 @@
 
 // Application state machine for Snake game
 enum class AppState {
-    StartMenu,   // Welcome screen with mode selection
-    LevelMode,   // Current implemented level-based mode
-    PortalMode,  // Portal mode (to be implemented)
-    ScoreMode,   // Endless score‑attack mode (to be implemented)
-    Exit,        // Terminate application
-    EnergyTrackMode
+    StartMenu,   
+    LevelMode, 
+    EnergyTrackMode,
+    PortalMode,  
+    ScoreMode,   
+    Exit    
 };
